c93f6d0337c53d16a9367a7e7f47d6a4b1fb0540
amanverma.verma530@gmail.com <amanverma.verma530@gmail.com>
2024-06-08 15:06:15 +0530

    home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className='responsive-table'>
+        <div className='header'>Header 1</div>
+        <div className='header'>Header 2</div>
+        <div className='header'>Header 3</div>
+        <div className='header'>Header 4</div>
+        <div>Row 1, Cell 1</div>
+        <div>Row 1, Cell 2</div>
+        <div>Row 1, Cell 3</div>
+        <div>Row 1, Cell 4</div>
+        <div>Row 2, Cell 1</div>
+        <div>Row 2, Cell 2</div>
+        <div>Row 2, Cell 3</div>
+        <div>Row 2, Cell 4</div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
